43bd13a2c1b6f41611034eb8ceec3254e7e17ab8
Daniel Gray <dg@danielgray.me>
2016-11-05 15:28:00 +0000

    New version!
    Here it is!

---
+++ redirect.js
@@ -0,0 +1,7 @@
+//Copyright Daniel Gray 2016. Refer to licence for more info
+
+document.getElementById("contentreplace").innerHTML = "Content: CONTENTNAMEGOESHERE";
+setTimeout(function(){
+    window.location = "Websiteofchoice";
+}, 7000);
+
